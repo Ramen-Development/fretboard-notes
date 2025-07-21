226e52d0aa605a15381dac2c9951895d5f66d3e6
Gustavo P. <piloalucard@gmail.com>
2025-07-20 21:37:33 -0600

    update: data structure on instrument

--- lib/model/instrument.dart
+++ lib/model/instrument.dart
@@ -4,14 +4,7 @@ enum InstrumentType { guitar, bass, ukulele, banjo, other }
 
 class Instrument {
   final InstrumentType type;
-  final String name;
-  final int strings;
   final Tuning tuning;
 
-  Instrument({
-    required this.type,
-    required this.name,
-    required this.strings,
-    required this.tuning,
-  });
+  Instrument({required this.type, required this.tuning});
 }
